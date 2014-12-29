7bc24869e76bf6a57d2558715b61895fa1fd8032
inotaku <i_no_u_u@yahoo.co.jp>
2014-12-29 16:09:42 +0900

    ドメインクラス更新

--- Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Author.java
+++ Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Author.java
@@ -2,63 +2,44 @@ package com.as.springbook.domain;
 
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "author")
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "books")
 public class Author {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "author_id")
 	private long authorId;
 
-	@Column(name = "first_name")
 	private String firstName;
 
 	@Column(name = "last_name")
 	private String lastName;
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY,mappedBy="author")
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "authorId")
 	private List<Book> books;
-
-	public long getAuthorId() {
-		return authorId;
-	}
-
-	public void setAuthorId(long authorId) {
-		this.authorId = authorId;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public List<Book> getBooks() {
-		return books;
-	}
-
-	public void setBooks(List<Book> books) {
-		this.books = books;
-	}
-
-	@Override
-	public String toString() {
-		return "Author [authorId=" + authorId + ", firstName=" + firstName
-				+ ", lastName=" + lastName + "]";
-	}
 
 }
--- Ch4/Booklibrary/src/main/java/com/as/springbook/domain/NewBook.java
+++ Ch4/Booklibrary/src/main/java/com/as/springbook/domain/NewBook.java
@@ -1,26 +1,17 @@
 package com.as.springbook.domain;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "books")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Book {
+public class NewBook {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,9 +21,7 @@ public class Book {
 
 	private int price;
 
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "authorId")
-	private Author author;
-
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Person person;
 }
