3913662db5c81988770333a30ad45550ed75305a
inotaku <i_no_u_u@yahoo.co.jp>
2014-12-29 16:12:57 +0900

    テスト更新

--- Ch4/Booklibrary/src/test/java/com/as/springbook/service/BookServiceImplTest.java
+++ Ch4/Booklibrary/src/test/java/com/as/springbook/service/BookServiceImplTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.as.springbook.BooklibraryApplication;
 import com.as.springbook.domain.Author;
@@ -12,10 +14,15 @@ import com.as.springbook.domain.Book;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes=BooklibraryApplication.class)
+@Transactional
+/*@TransactionConfiguration(defaultRollback=false)*/
 public class BookServiceImplTest {
 	
 	@Autowired
 	private BookService bookService;
+	
+	@Autowired
+	private AuthorService authorService;
 
 	@Test
 	public void testFindAll() {
@@ -23,20 +30,34 @@ public class BookServiceImplTest {
 
 	@Test
 	public void testFindOne() {
+		/*System.out.println(authorService.findAll());
+		System.out.println(bookService.findAll());*/
 	}
+	
+	
 
 	@Test
 	public void testBookCreate() {
 		Author author = new Author();
+		author.setAuthorId(1);
 		author.setFirstName("abc");
 		author.setLastName("xyz");
+		authorService.create(author);
+		
 		Book book = new Book();
 		book.setBookId(1);
 		book.setPrice(1000);
 		book.setTitle("初めてのspring");
-		book.setAuthor(author);
+		book.setAuthor(authorService.findOne(1));
 		bookService.create(book);
+		System.out.println(authorService.findAll());
 		System.out.println(bookService.findAll());
+		System.out.println("---");
+		Author author2 = new Author();
+		author2.setBooks(bookService.findAll());
+		authorService.create(author2);
+		
+		System.out.println(authorService.findOne(2).getBooks());
 		
 	}
 
