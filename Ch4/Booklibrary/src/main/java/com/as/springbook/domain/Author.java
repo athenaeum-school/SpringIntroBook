14d3c6d4cf6b593738902d23a4419173c994fed0
inotaku <i_no_u_u@yahoo.co.jp>
2015-01-04 14:53:45 +0900

    ドメインクラス更新

--- Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Author.java
+++ Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Author.java
@@ -1,6 +1,9 @@
 package com.as.springbook.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -22,12 +25,14 @@ import lombok.ToString;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "authors")
+@Table(name = "author")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString(exclude = "books")
 public class Author {
+
+
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -35,12 +40,11 @@ public class Author {
 
 	private String firstName;
 
-	@Column(name = "last_name")
 	private String lastName;
 
 	@JsonIgnore
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-	@JoinColumn(name = "authorId")
-	private List<Book> books;
+	@ManyToMany(mappedBy = "authors",cascade=CascadeType.ALL)
+	private List<Book> books = new ArrayList<Book>();
+	
 
 }
--- Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Book.java
+++ Ch4/Booklibrary/src/main/java/com/as/springbook/domain/Book.java
@@ -1,12 +1,20 @@
 package com.as.springbook.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,7 +24,7 @@ import lombok.NoArgsConstructor;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "books")
+@Table(name = "book")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -30,9 +38,9 @@ public class Book {
 
 	private int price;
 
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "authorId")
-	private Author author;
+	@ManyToMany(cascade=CascadeType.ALL)
+	private List<Author> authors = new ArrayList<Author>();
+	
 
+	
 }
