bba4b807b4b2b867b8587a764ab0737bb959befc
inotaku <i_no_u_u@yahoo.co.jp>
2015-01-08 16:36:10 +0900

    ブックタイトル・著者名検索機能追加

--- Ch4/SpringBoot/LibraryApplication/src/main/java/com/as/springbook/repository/BookRepository.java
+++ Ch4/SpringBoot/LibraryApplication/src/main/java/com/as/springbook/repository/BookRepository.java
@@ -7,7 +7,8 @@ package com.as.springbook.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.as.springbook.domain.Book;
@@ -16,4 +17,10 @@ import com.as.springbook.domain.Book;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 	Page<Book> findByTitleLike(String title,Pageable pageable);
+	
+	@Query("select distinct b from Book b left outer join b.authors a "
+			+ "where b.title like %:search% or a.firstName like %:search% "
+			+ "or a.lastName like %:search%")
+	Page<Book> findByTitleOrAuthorFirstNameOrAuthorLastName(@Param("search") String search,
+			Pageable pageable);
 }
--- Ch4/SpringBoot/LibraryApplication/src/main/java/com/as/springbook/service/BookServiceImpl.java
+++ Ch4/SpringBoot/LibraryApplication/src/main/java/com/as/springbook/service/BookServiceImpl.java
@@ -5,7 +5,6 @@
 package com.as.springbook.service;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -77,7 +76,7 @@ public class BookServiceImpl implements BookService {
 			sort = new Sort(Direction.DESC, title);
 		else
 			sort = null;
-		return bookRepository.findByTitleLike("%" + search + "%",
+		return bookRepository.findByTitleOrAuthorFirstNameOrAuthorLastName(search, 
 				new PageRequest(Integer.valueOf(page), Integer.valueOf(size),
 						sort));
 
